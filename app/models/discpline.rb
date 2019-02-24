d407aeba0385e2d3043edfa62b5c7f57116f98b9
Brad Schrag <brad.schrag@gmail.com>
2019-02-23 22:37:35 -0600

    properly spells discipline

--- app/models/discpline.rb
+++
@@ -1,2 +0,0 @@
-class Discpline < ApplicationRecord
-end
--- db/migrate/20190224042647_create_discplines.rb
+++
@@ -1,9 +0,0 @@
-class CreateDiscplines < ActiveRecord::Migration[5.2]
-  def change
-    create_table :discplines do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end
