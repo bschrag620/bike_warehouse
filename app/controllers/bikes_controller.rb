54c4fab69d5b4cca039a0990f630512254cba5ad
Brad Schrag <brad.schrag@gmail.com>
2019-04-08 07:28:25 -0500

    removed binding.pry from controller

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -11,20 +11,7 @@ class BikesController < ApplicationController
 	end
 
 	def index
-		direction = params[:direction] || 'asc'
-		@category = params[:category] || 'manufacturer'
-		
-		if !params[:manufacturer_id].nil?
-			@man = Manufacturer.find(params[:manufacturer_id])
-			@bikes = Bike.manufacturer_match(@man.name)
-		elsif !params[:discipline_id].nil?
-			@discipline = Discipline.find(params[:discipline_id])
-			@bikes = Bike.discipline_match(@discipline.name)
-		else
-			@bikes = Bike.unique_bikes
-		end
-
-		@bikes = @bikes.ordered_by(@category, direction)
+		@bikes = Bike.unique_bikes
 		respond_to do |format|
 			format.html
 			format.json { render :json => @bikes, status: 200 }
