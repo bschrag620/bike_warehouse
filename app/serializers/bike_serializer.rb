f9ca684c5910e99d7a850a053fa55cfd9641c734
Brad Schrag <brad.schrag@gmail.com>
2019-04-15 07:09:16 -0500

    update bike serializer

--- app/serializers/bike_serializer.rb
+++ app/serializers/bike_serializer.rb
@@ -1,5 +1,5 @@
 class BikeSerializer < ActiveModel::Serializer
-  attributes :id, :year, :components,	:size, :part_number, :price, :color, :rating, :quantity
+  attributes :id, :year, :components, :size, :part_number, :price, :color, :rating, :quantity
 
   attribute :frame_name, key: :frame
   attribute :manufacturer_name, key: :manufacturer
